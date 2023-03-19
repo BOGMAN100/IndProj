42479eb7c20be303ec86ce116d541fd729591322
BOGMAN100 <vladorlov064@gmail.com>
2023-03-19 17:54:15 +0300

    Hi/Done change

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import telebot
 from dotenv import load_dotenv
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# import PyPDF2
-
 load_dotenv()
 # Создаем экземпляр бота
 bot = telebot.TeleBot(os.getenv('TOKEN'))
 page = 1
 count = 3
+stn = 1
 # кнопки с предметами
 sub_data = [InlineKeyboardButton(text='Математика', callback_data='math', ),
             InlineKeyboardButton(text='Русский', callback_data='rus'),
@@ -27,6 +26,15 @@ cor = ('math', 'rus', 'phys', 'geo', 'his', 'bio', 'lit', 'inf', 'ob', 'him', 'e
 vector = ''
 
 
+# def reset_stn(func):
+#     def wrapper(*args, **kwargs):
+#         global stn
+#         stn = 0
+#         return func(*args, **kwargs)
+#
+#     return wrapper
+#
+
 # обработка действий кнопок
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
@@ -34,6 +42,7 @@ def callback_query(call):
     global count
     global page
     global vector
+    global stn
     # Обработка кнопки - скрыть
     if req[0] == 'unseen':
         bot.delete_message(call.message.chat.id, call.message.message_id)
@@ -52,8 +61,11 @@ def callback_query(call):
         bot.delete_message(call.message.chat.id, call.message.message_id)
         bot.send_message(call.message.chat.id, "Документ загружается...")
         path = vector + req[0] + ".pdf"
-    #   path = path.replace("\\", "/") # замена для правильной работы с докером
+        path = path.replace("\\", "/")  # замена для правильной работы с докером
         bot.send_document(call.message.chat.id, open(path, 'rb'))
+        page = 1
+        stn = 0
+        start(call)
 
     # Обработка кнопки - вперед
     elif req[0] == 'next-page':
@@ -92,6 +104,7 @@ def callback_query(call):
 def start(m):
     global count
     global page
+    global stn
     markup = InlineKeyboardMarkup()
     markup.add(sub_data[page - 1], sub_data[page])
     markup.add(sub_data[page + 1], sub_data[page + 2])
@@ -100,8 +113,17 @@ def start(m):
                InlineKeyboardButton(text=f'{page}/{count}', callback_data=f' '),
                InlineKeyboardButton(text=f'Вперёд --->', callback_data=f'next-page')
                )
-    bot.send_message(m.from_user.id, "Привет!\nПо какому предмету нужен материал?", reply_markup=markup)
+    if stn == 1:
+        bot.send_message(m.from_user.id, "Привет!\nПо какому предмету нужен материал?", reply_markup=markup) # не знаю почему наоборот, но да
+        stn = 0
+    elif stn == 0:
+        bot.send_message(m.from_user.id, "Готово!\nЧто-нибудь ещё?", reply_markup=markup)
+        stn = 1
 
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True, interval=0)
+    while True:
+        try:
+            bot.polling(none_stop=True, interval=0)
+        except:
+            pass
