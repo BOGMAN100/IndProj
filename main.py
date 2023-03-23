10d714515234f5e4fc5db591676dfcd5189ab13d
BOGMAN100 <vladorlov064@gmail.com>
2023-03-23 23:37:30 +0300

    Add EGE ( костысь с отображением OGE/EGE снизу)

--- main.py
+++ main.py
@@ -30,6 +30,27 @@ cor = ('math', 'rus', 'phys', 'geo', 'his', 'bio', 'lit', 'inf', 'ob', 'him', 'e
 vector = ''
 
 
+def start(m):
+    global count
+    global page
+    global stn
+    markup = InlineKeyboardMarkup()
+    markup.add(sub_data[page - 1], sub_data[page])
+    markup.add(sub_data[page + 1], sub_data[page + 2])
+    markup.add(InlineKeyboardButton(text='Скрыть', callback_data='unseen'))
+    markup.add(InlineKeyboardButton(text=f'<--- Назад', callback_data=f'back-page'),
+               InlineKeyboardButton(text=f'{page}/{count}', callback_data=f' '),
+               InlineKeyboardButton(text=f'Вперёд --->', callback_data=f'next-page')
+               )
+    if stn == 1:
+        bot.send_message(m.from_user.id, f"По какому предмету нужен материал?\n            {exam_name}",
+                         reply_markup=markup)  # не знаю почему наоборот, но да
+        stn = 0
+    elif stn == 0:
+        bot.send_message(m.from_user.id, f"Готово!\nЧто-нибудь ещё?\n            {exam_name}", reply_markup=markup)
+        stn = 1
+
+
 # обработка действий кнопок
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
@@ -40,15 +61,24 @@ def callback_query(call):
     global stn
     global sub
     global fil
+    global exam_name
+    global fo_func
     # Обработка кнопки - скрыть
     if req[0] == 'unseen':
         bot.delete_message(call.message.chat.id, call.message.message_id)
         page = 1
-
+    elif req[0] == 'exEGE':
+        exam_name = "EGE"
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        start(fo_func)
+    elif req[0] == "exOGE":
+        exam_name = "OGE"
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        start(fo_func)
 
     elif any([req[0] == x for x in cor]):
         sub = req[0]  # название предмета для вывода
-        vector = 'OGE\\' + req[0] + '\\'  # в папке с каким предметом искать
+        vector = exam_name + '\\' + req[0] + '\\'  # в папке с каким предметом искать
         markup = InlineKeyboardMarkup()
         markup.add(InlineKeyboardButton(text="Демоверсия", callback_data='demo'),
                    InlineKeyboardButton(text="Спецификатор", callback_data="spec"),
@@ -61,7 +91,8 @@ def callback_query(call):
     elif any([req[0] == x for x in ['demo', 'spec', 'kodif']]):
         fil = req[0]  # тип документа для вывода
         bot.delete_message(call.message.chat.id, call.message.message_id)
-        message = bot.send_message(call.message.chat.id, file_name[fil] + " по " + sub_name[sub] + " загружается...")
+        message = bot.send_message(call.message.chat.id, file_name[fil] + " " + exam_name + " по " + sub_name[sub]
+                                   + " загружается...")
         # bot.send_message(call.message.chat.id, file_name[fil] + " по " + sub_name[sub] + " загружается...")
         path = vector + req[0] + ".pdf"
         path = path.replace("\\", "/")  # замена для правильной работы с докером
@@ -84,7 +115,7 @@ def callback_query(call):
             markup.add(InlineKeyboardButton(text=f'<--- Назад', callback_data=f'back-page'),
                        InlineKeyboardButton(text=f'{page}/{count}', callback_data=f' '),
                        InlineKeyboardButton(text=f'Вперёд --->', callback_data=f'next-page'))
-            bot.edit_message_text(f'По какому предмету нужен материал?', reply_markup=markup,
+            bot.edit_message_text(f'По какому предмету нужен материал?\n            {exam_name}', reply_markup=markup,
                                   chat_id=call.message.chat.id,
                                   message_id=call.message.message_id)
 
@@ -103,7 +134,8 @@ def callback_query(call):
                 markup.add(InlineKeyboardButton(text=f'<--- Назад', callback_data=f'back-page'),
                            InlineKeyboardButton(text=f'{page}/{count}', callback_data=f' '),
                            InlineKeyboardButton(text=f'Вперёд --->', callback_data=f'next-page'))
-                bot.edit_message_text(f'По какому предмету нужен материал?', reply_markup=markup,
+                bot.edit_message_text(f'По какому предмету нужен материал?\\            *{exam_name}*',
+                                      reply_markup=markup,
                                       chat_id=call.message.chat.id,
                                       message_id=call.message.message_id)
             # убирает ошибку *сообщение не было изменено* ( в идеале перехватывать только ее )
@@ -112,26 +144,15 @@ def callback_query(call):
 
 
 # Обработчик входящих сообщений
+
 @bot.message_handler(content_types=['text'])
-def start(m):
-    global count
-    global page
-    global stn
+def exam(ch):
+    global fo_func
     markup = InlineKeyboardMarkup()
-    markup.add(sub_data[page - 1], sub_data[page])
-    markup.add(sub_data[page + 1], sub_data[page + 2])
-    markup.add(InlineKeyboardButton(text='Скрыть', callback_data='unseen'))
-    markup.add(InlineKeyboardButton(text=f'<--- Назад', callback_data=f'back-page'),
-               InlineKeyboardButton(text=f'{page}/{count}', callback_data=f' '),
-               InlineKeyboardButton(text=f'Вперёд --->', callback_data=f'next-page')
-               )
-    if stn == 1:
-        bot.send_message(m.from_user.id, "Привет!\nПо какому предмету нужен материал?",
-                         reply_markup=markup)  # не знаю почему наоборот, но да
-        stn = 0
-    elif stn == 0:
-        bot.send_message(m.from_user.id, "Готово!\nЧто-нибудь ещё?", reply_markup=markup)
-        stn = 1
+    markup.add(InlineKeyboardButton(text='ЕГЭ', callback_data='exEGE'))
+    markup.add(InlineKeyboardButton(text='ОГЭ', callback_data='exOGE'))
+    fo_func = ch
+    bot.send_message(ch.from_user.id, "Привет!\nПо какому экзамену нужен материал?", reply_markup=markup)
 
 
 if __name__ == '__main__':
