51cd2c996748106860e09c6710ca505e842793a9
BOGMAN100 <vladorlov064@gmail.com>
2023-03-24 11:03:58 +0300

    little fix

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def callback_query(call):
         page = 1
         stn = 0
         bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id,
-                              text=file_name[fil] + " по " + sub_name[sub] + ".")
+                              text=file_name[fil] + " " + exam_name + " по " + sub_name[sub] + ".")
         start(call)
 
     # Обработка кнопки - вперед
